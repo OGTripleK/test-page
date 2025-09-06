e8cf44e0e099b5b6a8c9464ff31318b3966d32dd
Adminsitrator <smartsolutions@tcc-technology.com>
2025-09-07 00:49:34 +0700

    .

--- .github/cmd.md
+++
@@ -1,2 +0,0 @@
-pnpm create next-app@15.1.6 test-page .
-
---
+++ test-page/src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import {Button} from "@heroui/react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Button>Click me</Button>
+    </div>
+  );
+}
